e951426c742930235fac0a17bf5a1fbcab051661
Semyon <fpm.korostik@bsu.by>
2023-03-16 21:25:25 +0300

    added json property

--- src/main/java/com/example/weatherapp/dto/WeatherDto.java
+++ src/main/java/com/example/weatherapp/dto/WeatherDto.java
@@ -1,29 +1,21 @@
 package com.example.weatherapp.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 @Builder
 public class WeatherDto implements Serializable {
     private final Integer temperature;
+    @JsonProperty("wind_speed")
     private final Integer windSpeed;
     private final Integer pressure;
     private final Integer humidity;
     private final String condition;
     private final String location;
-
-    @JsonCreator
-    public WeatherDto(Integer temperature, Integer windSpeed, Integer pressure, Integer humidity, String condition, 
-                      String location) {
-        this.temperature = temperature;
-        this.windSpeed = windSpeed;
-        this.pressure = pressure;
-        this.humidity = humidity;
-        this.condition = condition;
-        this.location = location;
-    }
 }
