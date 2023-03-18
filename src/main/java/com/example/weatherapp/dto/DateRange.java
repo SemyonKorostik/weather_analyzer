af2935952fc72e6183ff552df4ac62c6c9247726
Semyon <fpm.korostik@bsu.by>
2023-03-18 21:45:02 +0300

    added deserialization of json for dates

--- src/main/java/com/example/weatherapp/dto/DateRange.java
+++ src/main/java/com/example/weatherapp/dto/DateRange.java
@@ -1,6 +1,8 @@
 package com.example.weatherapp.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
@@ -19,9 +21,11 @@ public class DateRange implements Serializable {
     @NotNull
     @PastOrPresent
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate from;
     @NotNull
     @PastOrPresent
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate to;
 }
